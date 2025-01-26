e889d63ef9450cdb3dcee64c412d22a2da94067b
SuperSkidder <SuperSkidder@outlook.com>
2025-01-26 14:21:14 +0800

    fix: fix logo container scale

--- v1.8.9/src/main/java/top/fpsmaster/forge/mixin/MixinGuiContainer.java
+++ v1.8.9/src/main/java/top/fpsmaster/forge/mixin/MixinGuiContainer.java
@@ -5,11 +5,13 @@ import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.inventory.Container;
 import net.minecraft.util.ResourceLocation;
+import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import top.fpsmaster.features.impl.interfaces.ClientSettings;
 import top.fpsmaster.utils.render.Render2DUtils;
 
 @Mixin(GuiContainer.class)
@@ -23,10 +25,18 @@ public class MixinGuiContainer {
     protected int guiLeft;
     @Shadow
     protected int guiTop;
+
     @Inject(method = "drawScreen", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/inventory/GuiContainer;drawGuiContainerBackgroundLayer(FII)V"))
-    public void logo(int mouseX, int mouseY, float partialTicks, CallbackInfo ci){
+    public void logo(int mouseX, int mouseY, float partialTicks, CallbackInfo ci) {
         ScaledResolution sr = new ScaledResolution(Minecraft.getMinecraft());
-        Render2DUtils.drawImage(new ResourceLocation("client/gui/settings/logo.png"), 0, sr.getScaledHeight() - 32, 163 / 2f, 32, -1);
+        GL11.glPushMatrix();
+        Render2DUtils.fixScale();
+        if (ClientSettings.Companion.getFixedScale().getValue()) {
+            Render2DUtils.drawImage(new ResourceLocation("client/gui/settings/logo.png"), 0, (float) sr.getScaledHeight() * sr.getScaleFactor() / 2 - 32, 163 / 2f, 32, -1);
+        } else {
+            Render2DUtils.drawImage(new ResourceLocation("client/gui/settings/logo.png"), 0, (float) sr.getScaledHeight() - 32, 163 / 2f, 32, -1);
+        }
+        GL11.glPopMatrix();
     }
 
 }
