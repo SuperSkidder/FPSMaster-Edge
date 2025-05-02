dee76051394b78fe5a9ebe91ad71610980fa0e59
SuperSkidder <SuperSkidder@outlook.com>
2025-05-02 11:45:11 +0800

    feat: 推荐服务器

--- shared/java/top/fpsmaster/ui/mc/GuiMultiplayer.java
+++ shared/java/top/fpsmaster/ui/mc/GuiMultiplayer.java
@@ -1,6 +1,10 @@
 package top.fpsmaster.ui.mc;
 
 import com.google.common.collect.Lists;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import net.minecraft.client.gui.*;
 import net.minecraft.client.multiplayer.ServerData;
 import net.minecraft.client.multiplayer.ServerList;
@@ -17,11 +21,14 @@ import org.lwjgl.opengl.GL11;
 import top.fpsmaster.FPSMaster;
 import top.fpsmaster.font.impl.UFontRenderer;
 import top.fpsmaster.interfaces.ProviderManager;
+import top.fpsmaster.modules.client.AsyncTask;
 import top.fpsmaster.ui.click.component.ScrollContainer;
 import top.fpsmaster.ui.common.GuiButton;
 import top.fpsmaster.ui.screens.mainmenu.MainMenu;
+import top.fpsmaster.utils.os.HttpRequest;
 import top.fpsmaster.utils.render.Render2DUtils;
 import top.fpsmaster.utils.render.ScaledGuiScreen;
+import top.fpsmaster.utils.thirdparty.github.UpdateChecker;
 import top.fpsmaster.wrapper.ChatFormattingProvider;
 
 import java.awt.*;
@@ -33,8 +40,12 @@ public class GuiMultiplayer extends ScaledGuiScreen {
     private ServerData selectedServer;
     private static final Logger logger = LogManager.getLogger();
     private final List<ServerData> servers = Lists.newArrayList();
+    private final List<ServerListEntry> serverListDisplay = Lists.newArrayList();
     private final List<ServerListEntry> serverListInternet = Lists.newArrayList();
+    private static List<ServerListEntry> serverListRecommended = Lists.newArrayList();
     public final OldServerPinger oldServerPinger = new OldServerPinger();
+
+    private Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
     String action = "";
 
@@ -87,6 +98,19 @@ public class GuiMultiplayer extends ScaledGuiScreen {
         for (ServerData server : servers) {
             this.serverListInternet.add(new ServerListEntry(this, server));
         }
+        serverListDisplay.clear();
+        serverListDisplay.addAll(serverListInternet);
+        if (serverListRecommended.size() == 0) {
+            AsyncTask asyncTask = new AsyncTask(100);
+            asyncTask.runnable(() -> {
+                String s = HttpRequest.get("https://service.fpsmaster.top/getServers");
+                System.out.println(s);
+                JsonObject jsonObject = gson.fromJson(s, JsonObject.class);
+                jsonObject.get("data").getAsJsonArray().forEach(e -> {
+                    this.serverListRecommended.add(new ServerListEntry(this, new ServerData(e.getAsJsonObject().get("name").getAsString() + " - 推荐服务器", e.getAsJsonObject().get("address").getAsString(), false)));
+                });
+            });
+        }
     }
 
     @Override
@@ -135,6 +159,8 @@ public class GuiMultiplayer extends ScaledGuiScreen {
     }
 
     ScrollContainer scrollContainer = new ScrollContainer();
+    int tab = 0;
+
 
     @Override
     public void render(int mouseX, int mouseY, float partialTicks) {
@@ -146,10 +172,9 @@ public class GuiMultiplayer extends ScaledGuiScreen {
         title.drawCenteredString("多人游戏", width / 2f, 16, -1);
 
         Render2DUtils.drawOptimizedRoundedRect((width - 180) / 2f, 30, 180, 24, 3, new Color(255, 255, 255, 80).getRGB());
-        Render2DUtils.drawOptimizedRoundedRect((width - 176) / 2f, 32, 86, 20, 3, new Color(113, 127, 254).getRGB());
+        Render2DUtils.drawOptimizedRoundedRect((width - 176) / 2f + 90 * tab, 32, 86, 20, 3, new Color(113, 127, 254).getRGB());
         FPSMaster.fontManager.s16.drawCenteredString("服务器列表", (width - 90) / 2f, 36, -1);
         FPSMaster.fontManager.s16.drawCenteredString("推荐服务器", (width + 90) / 2f, 36, -1);
-
 
         GL11.glPushMatrix();
         GL11.glEnable(GL11.GL_SCISSOR_TEST);
@@ -158,7 +183,7 @@ public class GuiMultiplayer extends ScaledGuiScreen {
             float y = 70 + scrollContainer.getScroll();
             Render2DUtils.drawOptimizedRoundedRect((width - 400) / 2f, y - 10, 400, height - y, 5, new Color(0, 0, 0, 100).getRGB());
             int index = 0;
-            for (ServerListEntry server : serverListInternet) {
+            for (ServerListEntry server : serverListDisplay) {
                 if (server.getServerData() == null) {
                     return;
                 }
@@ -173,7 +198,7 @@ public class GuiMultiplayer extends ScaledGuiScreen {
                 index++;
                 y += 58;
             }
-            scrollContainer.setHeight(y - 70 - scrollContainer.getScroll());
+            scrollContainer.setHeight(y - 50 - scrollContainer.getScroll());
         });
         GL11.glDisable(GL11.GL_SCISSOR_TEST);
         GL11.glPopMatrix();
@@ -219,10 +244,24 @@ public class GuiMultiplayer extends ScaledGuiScreen {
         back.mouseClick(mouseX, mouseY, mouseButton);
 
 
-        int y = 80;
+        Render2DUtils.drawOptimizedRoundedRect((width - 180) / 2f, 30, 180, 24, 3, new Color(255, 255, 255, 80).getRGB());
+        Render2DUtils.drawOptimizedRoundedRect((width - 176) / 2f, 32, 86, 20, 3, new Color(113, 127, 254).getRGB());
+        FPSMaster.fontManager.s16.drawCenteredString("服务器列表", (width - 90) / 2f, 36, -1);
+        FPSMaster.fontManager.s16.drawCenteredString("推荐服务器", (width + 90) / 2f, 36, -1);
 
+        if (Render2DUtils.isHovered((width - 180) / 2f, 30, 90, 24, mouseX, mouseY)) {
+            tab = 0;
+            serverListDisplay.clear();
+            serverListDisplay.addAll(serverListInternet);
+        } else if (Render2DUtils.isHovered((width) / 2f, 30, 90, 24, mouseX, mouseY)) {
+            tab = 1;
+            serverListDisplay.clear();
+            serverListDisplay.addAll(serverListRecommended);
+        }
+
+        int y = 80;
         int index = 0;
-        for (ServerListEntry server : serverListInternet) {
+        for (ServerListEntry server : serverListDisplay) {
             if (server.getServerData() == null) {
                 return;
             }
@@ -236,6 +275,7 @@ public class GuiMultiplayer extends ScaledGuiScreen {
             index++;
             y += 54;
         }
+
 
     }
 
