9425048c6a315266ac2f9d162dc1a2bc30757085
SuperSkidder <SuperSkidder@qq.com>
2025-07-16 19:46:31 +0800

    fix: completely fixed gui position

--- shared/java/top/fpsmaster/utils/render/ScaledGuiScreen.java
+++ shared/java/top/fpsmaster/utils/render/ScaledGuiScreen.java
@@ -1,5 +1,6 @@
 package top.fpsmaster.utils.render;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.ScaledResolution;
 import org.lwjgl.opengl.GL11;
@@ -14,17 +15,29 @@ public class ScaledGuiScreen extends GuiScreen {
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         super.drawScreen(mouseX, mouseY, partialTicks);
-        ScaledResolution sr = new ScaledResolution(mc);
         GL11.glPushMatrix();
+        int realMouseX = mouseX * scaleFactor / 2;
+        int realMouseY = mouseY * scaleFactor / 2;
         scaleFactor = Render2DUtils.fixScale();
         float[] bounds = Render2DUtils.getFixedBounds();
         guiWidth = bounds[0];
         guiHeight = bounds[1];
-        int realMouseX = mouseX * scaleFactor / 2;
-        int realMouseY = mouseY * scaleFactor / 2;
-
         render(realMouseX, realMouseY, partialTicks);
         GL11.glPopMatrix();
+    }
+
+    @Override
+    public void onResize(Minecraft mcIn, int w, int h) {
+        super.onResize(mcIn, w, h);
+        scaleFactor = Render2DUtils.fixScale();
+        float[] bounds = Render2DUtils.getFixedBounds();
+        guiWidth = bounds[0];
+        guiHeight = bounds[1];
+    }
+
+    @Override
+    public void initGui() {
+        super.initGui();
     }
 
     @Override
