3a93c5a84807f085eda5e0b01101e876a1a58fe1
vlouboos <vlouyearlinjinhua@outlook.com>
2025-07-25 21:40:18 +0800

    Optimize code

--- shared/java/top/fpsmaster/features/impl/utility/AutoGG.java
+++ shared/java/top/fpsmaster/features/impl/utility/AutoGG.java
@@ -1,5 +1,7 @@
 package top.fpsmaster.features.impl.utility;
 
+import net.minecraft.event.ClickEvent;
+import net.minecraft.util.IChatComponent;
 import net.minecraft.util.StringUtils;
 import top.fpsmaster.FPSMaster;
 import top.fpsmaster.event.Subscribe;
@@ -11,9 +13,6 @@ import top.fpsmaster.features.settings.impl.ModeSetting;
 import top.fpsmaster.features.settings.impl.NumberSetting;
 import top.fpsmaster.features.settings.impl.TextSetting;
 import top.fpsmaster.interfaces.ProviderManager;
-import top.fpsmaster.modules.logger.ClientLogger;
-import top.fpsmaster.ui.notification.Notification;
-import top.fpsmaster.ui.notification.NotificationManager;
 import top.fpsmaster.utils.Utility;
 
 public class AutoGG extends Module {
@@ -21,9 +20,9 @@ public class AutoGG extends Module {
     public NumberSetting delay = new NumberSetting("DelayToPlay", 5, 0, 10, 1, () -> autoPlay.getValue());
     public TextSetting message = new TextSetting("Message", "gg");
     public ModeSetting servers = new ModeSetting("Servers", 0, "hypxiel", "kkcraft");
+    public String[] hypixelTrigger = new String[]{"Reward Summary", "1st Killer", "Damage Dealt", "奖励总览", "击杀数第一名", "造成伤害"};
+    public String[] kkcraftTrigger = new String[]{"获胜者", "第一名杀手", "击杀第一名"};
 
-    public String hypixelTrigger = "Reward Summary;1st Killer;Damage Dealt;奖励总览;击杀数第一名;造成伤害";
-    public String kkcraftTrigger = "获胜者;第一名杀手;击杀第一名";
     public AutoGG() {
         super("AutoGG", Category.Utility);
         this.addSettings(autoPlay, delay, message, servers);
@@ -32,42 +31,40 @@ public class AutoGG extends Module {
     @Subscribe
     public void onPacket(EventPacket event) {
         if (event.type == EventPacket.PacketType.RECEIVE && ProviderManager.packetChat.isPacket(event.packet)) {
-            String componentValue = ProviderManager.packetChat.getChatComponent(event.packet).toString();
-            String chatMessage = ProviderManager.packetChat.getUnformattedText(event.packet);
-            boolean hasEndInformation = false;
+            IChatComponent componentValue = ProviderManager.packetChat.getChatComponent(event.packet);
+            String chatMessage = componentValue.getUnformattedText();
             switch (servers.getValue()) {
                 case 0:
-                    boolean hasPlayCommand = componentValue.contains("ClickEvent{action=RUN_COMMAND, value='/play ");
-                    for (String s : hypixelTrigger.split(";")) {
-                        hasEndInformation = StringUtils.stripControlCodes(chatMessage).contains(s);
-                        if (hasEndInformation) break;
+                    for (String s : hypixelTrigger) {
+                        if (StringUtils.stripControlCodes(chatMessage).contains(s)) {
+                            Utility.sendChatMessage("/ac " + message.getValue());
+                            break;
+                        }
                     }
-                    if (hasEndInformation) {
-                        Utility.sendChatMessage("/ac " + message.getValue());
-                    }
-                    if (hasPlayCommand) {
-                        if (autoPlay.getValue()) {
-                            FPSMaster.async.runnable(() -> {
+                    if (autoPlay.getValue()) {
+                        for (IChatComponent chatComponent : componentValue.getSiblings()) {
+                            ClickEvent clickEvent = chatComponent.getChatStyle().getChatClickEvent();
+                            if (clickEvent != null && clickEvent.getAction().equals(ClickEvent.Action.RUN_COMMAND) && clickEvent.getValue().trim().toLowerCase().startsWith("/play ")) {
                                 Utility.sendClientNotify("Sending you to the next game in " + delay.getValue() + " seconds");
-                                try {
-                                    Thread.sleep(delay.getValue().longValue() * 1000);
-                                } catch (InterruptedException e) {
-                                    throw new RuntimeException(e);
-                                }
-                                Utility.sendChatMessage(componentValue.substring(componentValue.indexOf("value='") + 7, componentValue.indexOf("'}")));
-                            });
+                                FPSMaster.async.runnable(() -> {
+                                    try {
+                                        Thread.sleep(delay.getValue().longValue() * 1000);
+                                        Utility.sendChatMessage(clickEvent.getValue());
+                                    } catch (InterruptedException e) {
+                                        throw new RuntimeException(e);
+                                    }
+                                });
+                            }
                         }
                     }
                     break;
                 case 1:
-                    for (String s : kkcraftTrigger.split(";")) {
-                        hasEndInformation = StringUtils.stripControlCodes(chatMessage).contains(s);
-                        if (hasEndInformation) break;
-                    }
-                    if (hasEndInformation) {
-                        Utility.sendChatMessage(message.getValue());
-                        if(autoPlay.getValue()) {
-                            Utility.sendClientNotify("AutoPlay is not supported at the moment in KKCraft");
+                    for (String s : kkcraftTrigger) {
+                        if (StringUtils.stripControlCodes(chatMessage).contains(s)) {
+                            Utility.sendChatMessage(message.getValue());
+                            if(autoPlay.getValue()) {
+                                Utility.sendClientNotify("AutoPlay is not supported at the moment in KKCraft");
+                            }
                         }
                     }
                     break;
